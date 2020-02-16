30d586245b345118ec22543ba10ae257453a2cf3
C.M.Cai <hzxsccm678335@163.com>
2020-02-17 02:34:31 +0800

    add q_learning

--- algorithm/Q_Learning.py
+++ algorithm/Q_Learning.py
@@ -38,20 +38,6 @@ def df_preprocess(path):
     price_columns = colnames[[col[-5:] == 'close' for col in colnames]]
     return dataframe, price_columns.to_list()
 
-df, price_columns = df_preprocess('./data/create_feature.csv')
-windows = 250
-env = MarketEnv(df=df, price_cols=price_columns, windows=windows,
-                initial_account_balance=10000., buy_fee=0.015, sell_fee=0.)
-n_actions = env.action_space.shape[0]
-policy_net = LSTM_DQN(input_size=df.shape[1], hidden_size=128, output_size=n_actions).to(device)
-target_net = LSTM_DQN(input_size=df.shape[1], hidden_size=128, output_size=n_actions).to(device)
-target_net.load_state_dict(policy_net.state_dict())
-target_net.eval()
-optimizer = optim.RMSprop(policy_net.parameters())
-
-
-Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
-
 
 class ReplayMemory(object):
 
@@ -77,11 +63,12 @@ class ReplayMemory(object):
 # 定义记忆长度为10000的replaymemory
 memory = ReplayMemory(10000)
 
-BATCH_SIZE = 128
+
+BATCH_SIZE = 256
 GAMMA = 0.999
 EPS_START = 0.9
-EPS_END = 0.05
-EPS_DECAY = 200
+EPS_END = 0.1
+EPS_DECAY = 30000
 TARGET_UPDATE = 10
 
 
@@ -98,11 +85,8 @@ def select_action(state1, state2, hold_rate):
         return hold_rate.to(device)
 
 
-episode_durations = []
 # 优化模型参数
 def optimize_model(memory):
-    #if len(memory) < BATCH_SIZE:
-    #    return
     transitions = memory.sample(BATCH_SIZE)
     batch = Transition(*zip(*transitions))
     # 对next_state进行拼接
@@ -114,7 +98,6 @@ def optimize_model(memory):
     # 对action 和 reward 拼接
     action_batch = torch.cat(batch.action)
     reward_batch = torch.cat(batch.reward).unsqueeze(1)
-
     mu_batch, sigma_batch, beta_batch = policy_net(env_state_batch, act_state_batch)
     state_action_values = -(action_batch - mu_batch).unsqueeze(1).bmm(sigma_batch).bmm(
         torch.transpose(sigma_batch, 1, 2)).bmm(torch.transpose((action_batch - mu_batch).unsqueeze(1), 1, 2)).squeeze(
@@ -126,35 +109,73 @@ def optimize_model(memory):
     optimizer.zero_grad()
     loss.backward()
     for param in policy_net.parameters():
-        param.grad.data.clamp_(-1, 1)
+        param.grad.data.clamp_(-10, 10)
     optimizer.step()
 
 
-num_episodes = 50
-for i_episode in range(num_episodes):
-    # Initialize the environment and state
+def test_interact(env):
+    net_list = []
     state1, state2 = env.reset()
-    for t in count():
+    while True:
         state1 = torch.from_numpy(state1).unsqueeze(0)
         state2 = torch.from_numpy(state2).unsqueeze(0)
-        hold_rate = torch.from_numpy(env.next_rate.astype(np.float32)).unsqueeze(0)
-        # Select and perform an action
-        action = select_action(state1, state2, hold_rate)
+        with torch.no_grad():
+            action = policy_net(state1, state2)[0]
         next_state, reward, done, _ = env.step(action.squeeze().detach().numpy())
-        reward = torch.tensor([reward], device=device)
-        memory.push((state1, state2), action, next_state, reward)
-        # Move to the next state
+        net_list.append(env.next_net)
         state1, state2 = next_state
-        if len(memory) > BATCH_SIZE:
-            optimize_model(memory)
         if done:
+            print('test process: \n')
             env.render()
-            episode_durations.append(t + 1)
-            #plot_durations()
             break
-    # Update the target network, copying all weights and biases in DQN
-    if i_episode % TARGET_UPDATE == 0:
-        target_net.load_state_dict(policy_net.state_dict())
-        torch.save(policy_net.state_dict(), "./model/Q_learning_model_%epoch.pt" % i_episode)
+    return np.array(net_list)/env.initial_account_balance-1
 
-env.render()
+
+def interact(env, num_episodes=50, target_update=10):
+    ret_df = pd.DataFrame(index=df.index[250:], dtype=np.float64)
+    for i_episode in range(num_episodes):
+        # Initialize the environment and state
+        state1, state2 = env.reset()
+        for t in count():
+            state1 = torch.from_numpy(state1).unsqueeze(0)
+            state2 = torch.from_numpy(state2).unsqueeze(0)
+            hold_rate = torch.from_numpy(env.next_rate.astype(np.float32)).unsqueeze(0)
+            # Select and perform an action
+            action = select_action(state1, state2, hold_rate)
+            next_state, reward, done, _ = env.step(action.squeeze().detach().numpy())
+            reward = torch.tensor([reward], device=device)
+            memory.push((state1, state2), action, next_state, reward)
+            # Move to the next state
+            state1, state2 = next_state
+            if len(memory) > BATCH_SIZE:
+                optimize_model(memory)
+            if done:
+                env.render()
+                break
+        # Update the target network, copying all weights and biases in DQN
+        if i_episode+1 % target_update == 0:
+            target_net.load_state_dict(policy_net.state_dict())
+            torch.save(policy_net.state_dict(), "./model/Q_learning_model_%epoch.pt" % i_episode)
+            ret_df['%sepoch'%i_episode] = test_interact(env)
+            ret_df.plot(title='Returns Curve')
+            plt.savefig('./image/ret/Q_learning.jpg')
+            plt.close()
+
+
+if __name__ == '__main__':
+
+    df, price_columns = df_preprocess('./data/create_feature.csv')
+    windows = 250
+    env = MarketEnv(df=df, price_cols=price_columns, windows=windows,
+                    initial_account_balance=10000., buy_fee=0.015, sell_fee=0.)
+    n_actions = env.action_space.shape[0]
+    policy_net = LSTM_DQN(input_size=df.shape[1], hidden_size=128, output_size=n_actions).to(device)
+    target_net = LSTM_DQN(input_size=df.shape[1], hidden_size=128, output_size=n_actions).to(device)
+    target_net.load_state_dict(policy_net.state_dict())
+    target_net.eval()
+    optimizer = optim.RMSprop(policy_net.parameters())
+    Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+    num_episodes = 100
+    TARGET_UPDATE = 10
+    interact(env, num_episodes=num_episodes, target_update=TARGET_UPDATE)
