6a57ae87875c2d6fec0a65bfc416bdd15dbb93bc
C.M. Cai <hzxsccm678335@163.com>
2020-03-29 16:11:22 +0800

    update pathwise_derivative algorithm

--- baselines/policy_network.py
+++ baselines/policy_network.py
@@ -107,7 +107,6 @@ class LSTM_DQN(nn.Module):
         return mu, sigma_matrix.reshape((-1, self.output_size, self.output_size)), sigma_vector
 
 
-
 class LSTM_A2C(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(LSTM_A2C, self).__init__()
@@ -122,6 +121,32 @@ class LSTM_A2C(nn.Module):
         self.hidden_layer2 = nn.Linear(64, 8)
         self.hidden_layer3 = nn.Linear(8,1)
 
+    def forward(self, env_state, act_state, action):
+        lstm_out, (h_n, c_n) = self.lstm(env_state)
+        cat_layer = torch.cat((lstm_out[:, -1, :], act_state), 1)
+
+        mu = F.relu(self.hidden_mu1(cat_layer))
+        mu = F.relu(self.hidden_mu2(mu))
+        mu = F.relu(self.hidden_mu3(mu))
+        mu = torch.softmax(self.hidden_mu4(mu), dim=1)
+
+        cat_layer = torch.cat((cat_layer, action, mu), 1)
+        q = F.selu(self.hidden_layer1(cat_layer))
+        q = F.selu(self.hidden_layer2(q))
+        q = self.hidden_layer3(q)
+        return mu, q
+
+
+class A2C_ACTOR(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(A2C_ACTOR, self).__init__()
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=2,
+                            bias=True, batch_first=True, dropout=0, bidirectional=False)
+        self.hidden_mu1 = nn.Linear(hidden_size+output_size, 64)
+        self.hidden_mu2 = nn.Linear(64, 32)
+        self.hidden_mu3 = nn.Linear(32, 16)
+        self.hidden_mu4 = nn.Linear(16, output_size)
+
     def forward(self, env_state, action_state):
         lstm_out, (h_n, c_n) = self.lstm(env_state)
         cat_layer = torch.cat((lstm_out[:, -1, :], action_state), 1)
@@ -130,9 +155,23 @@ class LSTM_A2C(nn.Module):
         mu = F.relu(self.hidden_mu2(mu))
         mu = F.relu(self.hidden_mu3(mu))
         mu = torch.softmax(self.hidden_mu4(mu), dim=1)
+        return mu
 
-        cat_layer = torch.cat((cat_layer, mu), 1)
+
+class A2C_QVALUE(nn.Module):
+    def __init__(self, input_size, hidden_size, action_size):
+        super(A2C_QVALUE, self).__init__()
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=2,
+                            bias=True, batch_first=True, dropout=0, bidirectional=False)
+        self.hidden_layer1 = nn.Linear(hidden_size + action_size*2, 64)
+        self.hidden_layer2 = nn.Linear(64, 8)
+        self.hidden_layer3 = nn.Linear(8,1)
+
+    def forward(self, env_state, action_state, action):
+        lstm_out, (h_n, c_n) = self.lstm(env_state)
+        cat_layer = torch.cat((lstm_out[:, -1, :], action_state), 1)
+        cat_layer = torch.cat((cat_layer, action), 1)
         q = F.selu(self.hidden_layer1(cat_layer))
         q = F.selu(self.hidden_layer2(q))
         q = self.hidden_layer3(q)
-        return mu, q
+        return q
