6b8592dd6dab665130283ee07cae5bdce789b252
cmcai0104 <hzxsccm678335@163.com>
2020-05-07 16:50:56 +0800

    add finance feature

--- algorithm/Pathwise_Derivative_Explore.py
+++ algorithm/Pathwise_Derivative_Explore.py
@@ -30,11 +30,34 @@ def df_preprocess(path):
     colnames = df.columns.to_list()
     colnames = list(set(colnames) - set(['000001.SH_pe_y', '000300.SH_pe_y', '000905.SH_pe_y', '399006.SZ_pe_y']))
     colnames = [col for col in colnames if (col[:6] != '399016')]
-    df = df[colnames].dropna(axis=0, how='all').fillna(method='ffill', axis=0).dropna(axis=0, how='any')
-    for ind in [5, 10, 20, 30, 40, 60, 70, 125, 250, 500, 750]:
-        df[[col + '_m' + str(ind) for col in colnames]] = df[colnames].rolling(window=ind, min_periods=1).mean()
-        df[[col + '_q' + str(ind) for col in colnames]] = df[colnames].rolling(window=ind, min_periods=1).apply(
+
+    for col in colnames:
+        # add moving average
+        df[col + '_ma7'] = df[col].rolling(window=7, min_periods=1).mean()
+        df[col + '_ma21'] = df[col].rolling(window=21, min_periods=1).mean()
+        # add macd
+        df[col + '_ema26'] = df[col].ewm(span=26).mean()
+        df[col + '_ema12'] = df[col].ewm(span=12).mean()
+        df[col + '_MACD'] = df[col + '_ema12'] - df[col + '_ema26']
+        # add bollinger bands
+        df[col + '_sd20'] = df[col].rolling(window=20).std()
+        df[col + 'upper_band'] = df[col + '_ma21'] + (df[col + '_sd20'] * 2)
+        df[col + 'lower_band'] = df[col + '_ma21'] - (df[col + '_sd20'] * 2)
+        # add ema
+        df[col + '_ema'] = df[col].ewm(com=0.5).mean()
+        # fourier transforms
+        fft_list = np.fft.fft(np.asarray(df[col].tolist()))
+        for num_ in [3, 6, 9, 100]:
+            fft_list_m = np.copy(fft_list)
+            fft_list_m[num_:-num_] = 0
+            df[col + '_fft%s' % num_] = np.fft.ifft(fft_list_m)
+    # add quantile
+    for num_ in [1, 2, 3, 5]:
+        df[[col + '_qu' + str(250 * num_) for col in colnames]] = df[colnames].rolling(window=(250 * num_),
+                                                                                       min_periods=50).apply(
             lambda x: len(x[x <= x[-1]]) / len(x), raw=True)
+
+    df = df[colnames].dropna(axis=0, how='all').fillna(method='ffill', axis=0).dropna(axis=0, how='any')
     price_columns = [col for col in colnames if (col[-5:] == 'close')]
     return df, price_columns
 
@@ -144,9 +167,11 @@ def test_interact(env):
 if __name__ == '__main__':
     df, price_columns = df_preprocess('./data/create_feature.csv')
     windows = 250
-    env = MarketEnv(df=df, price_cols=price_columns, windows=windows,
-                    initial_account_balance=10000., buy_fee=0.015, sell_fee=0.)
-    n_actions = env.action_space.shape[0]
+    train_env = MarketEnv(df=df.iloc[:1500, :], price_cols=price_columns, windows=windows,
+                          initial_account_balance=10000., buy_fee=0.015, sell_fee=0.)
+    test_env = MarketEnv(df=df, price_cols=price_columns, windows=windows,
+                         initial_account_balance=10000., buy_fee=0.015, sell_fee=0.)
+    n_actions = train_env.action_space.shape[0]
     policy_net = ACTOR_QVALUE(input_size=df.shape[1], hidden_size=128, action_size=n_actions).to(device)
     target_net = ACTOR_QVALUE(input_size=df.shape[1], hidden_size=128, action_size=n_actions).to(device)
     target_net.load_state_dict(policy_net.state_dict())
@@ -162,14 +187,14 @@ if __name__ == '__main__':
     loss_list = []
     for i_episode in range(num_episodes):
         # Initialize the environment and state
-        state1, state2 = env.reset()
+        state1, state2 = train_env.reset()
         for t in count():
             state1 = torch.from_numpy(state1).unsqueeze(0)
             state2 = torch.from_numpy(state2).unsqueeze(0)
-            hold_rate = torch.from_numpy(env.next_rate.astype(np.float32)).unsqueeze(0)
+            hold_rate = torch.from_numpy(train_env.next_rate.astype(np.float32)).unsqueeze(0)
             # Select and perform an action
             action = select_action(state1, state2, hold_rate)
-            next_state, reward, done, _ = env.step(action.squeeze().detach().numpy())
+            next_state, reward, done, _ = train_env.step(action.squeeze().detach().numpy())
             reward = torch.tensor([reward], device=device)
             memory.push((state1, state2), action, next_state, reward)
             # Move to the next state
@@ -182,12 +207,12 @@ if __name__ == '__main__':
                 target_net.load_state_dict(policy_net.state_dict())
             if done:
                 print('%s,  ' % i_episode, end=' ')
-                env.render()
+                train_env.render()
                 break
         # Update the target network, copying all weights and biases in DQN
         if (i_episode + 1) % TARGET_UPDATE == 0:
             torch.save(policy_net.state_dict(), "./model/pathwise_derivative_explore%s epoch.pt" % (i_episode + 1))
-            ret_df['%s epoch' % (i_episode + 1)] = test_interact(env)
+            ret_df['%s epoch' % (i_episode + 1)] = test_interact(test_env)
             ret_df.plot(title='Returns Curve')
             plt.savefig('./image/ret/pathwise_derivative_explore.jpg')
             plt.close()
--- algorithm/Q_Learning_Explore.py
+++ algorithm/Q_Learning_Explore.py
@@ -6,6 +6,7 @@ import math
 from collections import namedtuple
 import numpy as np
 import pandas as pd
+from statsmodels.tsa.arima_model import ARIMA
 import matplotlib.pyplot as plt
 from itertools import count
 import torch
@@ -30,11 +31,34 @@ def df_preprocess(path):
     colnames = df.columns.to_list()
     colnames = list(set(colnames) - set(['000001.SH_pe_y', '000300.SH_pe_y', '000905.SH_pe_y', '399006.SZ_pe_y']))
     colnames = [col for col in colnames if (col[:6] != '399016')]
-    df = df[colnames].dropna(axis=0, how='all').fillna(method='ffill', axis=0).dropna(axis=0, how='any')
-    for ind in [5, 10, 20, 30, 40, 60, 70, 125, 250, 500, 750]:
-        df[[col + '_m' + str(ind) for col in colnames]] = df[colnames].rolling(window=ind, min_periods=1).mean()
-        df[[col + '_q' + str(ind) for col in colnames]] = df[colnames].rolling(window=ind, min_periods=1).apply(
+
+    for col in colnames:
+        # add moving average
+        df[col + '_ma7'] = df[col].rolling(window=7, min_periods=1).mean()
+        df[col + '_ma21'] = df[col].rolling(window=21, min_periods=1).mean()
+        # add macd
+        df[col + '_ema26'] = df[col].ewm(span=26).mean()
+        df[col + '_ema12'] = df[col].ewm(span=12).mean()
+        df[col + '_MACD'] = df[col + '_ema12'] - df[col + '_ema26']
+        # add bollinger bands
+        df[col + '_sd20'] = df[col].rolling(window=20).std()
+        df[col + 'upper_band'] = df[col + '_ma21'] + (df[col + '_sd20'] * 2)
+        df[col + 'lower_band'] = df[col + '_ma21'] - (df[col + '_sd20'] * 2)
+        # add ema
+        df[col + '_ema'] = df[col].ewm(com=0.5).mean()
+        # fourier transforms
+        fft_list = np.fft.fft(np.asarray(df[col].tolist()))
+        for num_ in [3, 6, 9, 100]:
+            fft_list_m = np.copy(fft_list)
+            fft_list_m[num_:-num_] = 0
+            df[col + '_fft%s' % num_] = np.fft.ifft(fft_list_m)
+    # add quantile
+    for num_ in [1, 2, 3, 5]:
+        df[[col + '_qu' + str(250 * num_) for col in colnames]] = df[colnames].rolling(window=(250 * num_),
+                                                                                       min_periods=50).apply(
             lambda x: len(x[x <= x[-1]]) / len(x), raw=True)
+
+    df = df[colnames].dropna(axis=0, how='all').fillna(method='ffill', axis=0).dropna(axis=0, how='any')
     price_columns = [col for col in colnames if (col[-5:] == 'close')]
     return df, price_columns
 
@@ -138,14 +162,16 @@ def test_interact(env):
 if __name__ == '__main__':
     df, price_columns = df_preprocess('./data/create_feature.csv')
     windows = 250
-    env = MarketEnv(df=df, price_cols=price_columns, windows=windows,
-                    initial_account_balance=10000., buy_fee=0.015, sell_fee=0.)
-    n_actions = env.action_space.shape[0]
+    train_env = MarketEnv(df=df.iloc[:1500, :], price_cols=price_columns, windows=windows,
+                          initial_account_balance=10000., buy_fee=0.015, sell_fee=0.)
+    test_env = MarketEnv(df=df, price_cols=price_columns, windows=windows,
+                         initial_account_balance=10000., buy_fee=0.015, sell_fee=0.)
+    n_actions = train_env.action_space.shape[0]
     policy_net = LSTM_DQN(input_size=df.shape[1], hidden_size=128, output_size=n_actions).to(device)
     target_net = LSTM_DQN(input_size=df.shape[1], hidden_size=128, output_size=n_actions).to(device)
     target_net.load_state_dict(policy_net.state_dict())
     optimizer = optim.Adam(policy_net.parameters())
-    #scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=10)
+    # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=10)
     Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
     memory = ReplayMemory(30000)
     num_episodes = 50
@@ -155,14 +181,14 @@ if __name__ == '__main__':
     loss_list = []
     for i_episode in range(num_episodes):
         # Initialize the environment and state
-        state1, state2 = env.reset()
+        state1, state2 = train_env.reset()
         for t in count():
             state1 = torch.from_numpy(state1).unsqueeze(0)
             state2 = torch.from_numpy(state2).unsqueeze(0)
-            hold_rate = torch.from_numpy(env.next_rate.astype(np.float32)).unsqueeze(0)
+            hold_rate = torch.from_numpy(train_env.next_rate.astype(np.float32)).unsqueeze(0)
             # Select and perform an action
             action = select_action(state1, state2, hold_rate)
-            next_state, reward, done, _ = env.step(action.squeeze().detach().numpy())
+            next_state, reward, done, _ = train_env.step(action.squeeze().detach().numpy())
             reward = torch.tensor([reward], device=device)
             memory.push((state1, state2), action, next_state, reward)
             # Move to the next state
@@ -170,17 +196,17 @@ if __name__ == '__main__':
             if len(memory) >= (BATCH_SIZE * 5) and (t % 2 == 0):
                 loss = optimize_model(memory)
                 loss_list.append(loss.detach().numpy())
-                #scheduler.step(loss)
+                # scheduler.step(loss)
             if len(memory) >= (BATCH_SIZE * 5) and (t % 100 == 0):
                 target_net.load_state_dict(policy_net.state_dict())
             if done:
                 print('%s,  ' % i_episode, end=' ')
-                env.render()
+                train_env.render()
                 break
         # Update the target network, copying all weights and biases in DQN
         if (i_episode + 1) % TARGET_UPDATE == 0:
             torch.save(policy_net.state_dict(), "./model/q_learning_explore%s epoch.pt" % (i_episode + 1))
-            ret_df['%s epoch' % (i_episode + 1)] = test_interact(env)
+            ret_df['%s epoch' % (i_episode + 1)] = test_interact(test_env)
             ret_df.plot(title='Returns Curve')
             plt.savefig('./image/ret/Q_learning_explore.jpg')
             plt.close()
